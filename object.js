101bcc56fb32896cbd2af224c6e15d8ae90bc9b2
Kingsley Emoka <69216617+kingsleyEmoka@users.noreply.github.com>
2022-06-24 04:09:43 +0100

    Add files via upload

---
+++ object.js
@@ -0,0 +1,13 @@
+myInfo = {
+    name : "Emoka Kingsley </br>",
+    height : "5'9 </br>",
+    country : "Nigeria"
+};
+
+let name = myInfo.name;
+let height = myInfo.height;
+let country = myInfo.country;
+
+document.write(name);
+document.write(height);
+document.write(country);
